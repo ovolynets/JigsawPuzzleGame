3194a4ecd6835725ff51c88509535a3d9a76bb30
Oleksandr Volynets <oleksandr.volynets@gmail.com>
2019-02-15 19:50:41 +0100

    Updated to version 1.3

--- PuzzleGame/Model/GameSettings.cs
+++ PuzzleGame/Model/GameSettings.cs
@@ -12,7 +12,10 @@ namespace PuzzleGame.Model
             new Tuple<int, int>(2, 2),
             new Tuple<int, int>(3, 3),
             new Tuple<int, int>(4, 4),
-            new Tuple<int, int>(5, 5)
+            new Tuple<int, int>(5, 5),
+            new Tuple<int, int>(6, 6),
+            new Tuple<int, int>(7, 7),
+            new Tuple<int, int>(8, 8)
         };
 
         // Default size 3x3
